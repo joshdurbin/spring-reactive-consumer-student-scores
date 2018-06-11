2810bc08f38acd1229f168fdc3a9b2ab0f241a32
Josh Durbin <durbinjo593@gmail.com>
2018-06-10 22:30:39 -0700

    removing unused import

--- src/main/java/io/durbs/scores/ScoresApplication.java
+++ src/main/java/io/durbs/scores/ScoresApplication.java
@@ -2,10 +2,8 @@ package io.durbs.scores;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
-@EnableAsync
 public class ScoresApplication {
 
 	public static void main(String[] args) {
